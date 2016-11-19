cf3641b8fc97ef056e533a51f73069a12b5714ff
Kevin Chen <kevin.chen@veeva.com>
2016-11-19 18:16:58 -0500

    [POKER] Added Snap to the project and implemented sample on login page

--- public/js/Controllers/Login/Login.js
+++ public/js/Controllers/Login/Login.js
@@ -1,1 +1,85 @@
-// Login.js
+if (_.isUndefined(window.Controllers)) {
+    window.Controllers = {};
+}
+/**
+ * This defines the login controller.
+ *
+ * @class Login
+ */
+Controllers.Login = function(options) {
+    // define the appropriate defaults
+    this.options = _.defaults(options || {}, {
+        $el: null
+    });
+    /**
+     * This initializes the controller.
+     *
+     * @method init
+     */
+    this.init = function() {
+        var self = this;
+
+        // point to the correct binding element
+        self.options.$el = $("#Login");
+
+        // render the controller
+        self.render();
+    };
+    /**
+     * This renders the controller.
+     *
+     * @method render
+     */
+    this.render = function() {
+        var self = this;
+
+        // render the corresponding template
+        self.options.$el.render("js/Controllers/Login/views/view.ejs", {
+            // ...
+        });
+        // call the post-render method
+        self.postRender();
+    };
+    /**
+     * This occurs after the controller is rendered.
+     *
+     * @method postRender
+     */
+    this.postRender = function() {
+        var self = this;
+        var s = Snap("#logo");
+
+        // Now lets create another small circle:
+        var smallCircle = s.circle(100, 150, 70);
+        // Lets put this small circle and another one into a group:
+        var discs = s.group(smallCircle, s.circle(200, 150, 70));
+
+        Snap.load("images/cards.svg", function(fragment) {
+            fragment.selectAll("g").attr({
+                stroke: "#fafafa",
+                fill: "#fafafa"
+            })
+            discs.attr({
+                mask: fragment,
+                    stroke: "#fafafa",
+                    fill: "#fafafa"
+            });
+            discs.selectAll("circle").forEach(function(circle) {
+                console.log(circle);
+                circle.animate({r: 100}, 1000, mina.elastic);
+            });
+            s.append(fragment);
+        });
+
+    };
+    /**
+     * This destroys the controller.
+     *
+     * @method destroy
+     */
+    this.destroy = function() {
+        // ...
+    };
+    // initializes the contoller.
+    this.init();
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,39 @@
-// define the user data.
-var userId = "k39chen";
-var userData = {
-    id: "k39chen",
-    email: "k39chen@gmail.com",
-    firstName: "Kevin",
-    lastName: "Chen"
-};
-// add the user k39chen to the database
-db.ref("users/" + userId).set(userData);
+$(document).ready(function() {
+    // define the user data.
+    var userId = "k39chen";
+    var userData = {
+        id: "k39chen",
+        email: "k39chen@gmail.com",
+        firstName: "Kevin",
+        lastName: "Chen"
+    };
+    // add the user k39chen to the database
+    db.ref("users/" + userId).set(userData);
 
-db.ref("users").on("value", function(snap) {
-    var usersData = snap.val();
-    console.log(usersData);
-});
-
-db.ref("users/" + userId).on("value", function(snap) {
-    var userData = snap.val();
-    console.log(userData);
-    renderUser(userData);
-});
-
-function renderUser(userData) {
-    var $users = $("#users");
-    var $user = $("<div class='user' />").appendTo($users);
-
-    _.each(userData, function(value, property) {
-        var $row = $("<div class='row' />")
-            .append("<span class='property'>" + property + "</span>: ")
-            .append("<span class='value'>" + value + "</span>")
-            .appendTo($user)
+    db.ref("users").on("value", function(snap) {
+        var usersData = snap.val();
+        console.log(usersData);
     });
-}
-$("#app").render("js/Controllers/Home/views/view.ejs", {});
+
+    db.ref("users/" + userId).on("value", function(snap) {
+        var userData = snap.val();
+        console.log(userData);
+        renderUser(userData);
+    });
+
+    function renderUser(userData) {
+        var $users = $("#users");
+        var $user = $("<div class='user' />").appendTo($users);
+
+        _.each(userData, function(value, property) {
+            var $row = $("<div class='row' />")
+                .append("<span class='property'>" + property + "</span>: ")
+                .append("<span class='value'>" + value + "</span>")
+                .appendTo($user)
+        });
+    }
+
+
+    // start off with the login screen
+    var Login = new Controllers.Login();
+});
